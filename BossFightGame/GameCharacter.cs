41d215979004757c25f1242e639b18ed5a8bd2a6
imanahmedoslo <imanahmedimanahmed@gmail.com>
2023-05-16 00:10:49 +0200

    Add project files.

---
+++ BossFightGame/GameCharacter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BossFightGame
+{
+    public class GameCharacter
+    {
+        public int Health { get; set; }
+        public int Strength { get; set; }
+        public int Stamina { get; set; }
+
+        public GameCharacter(int health, int strength, int stamina) { 
+            this.Health = health;   
+            this.Strength = strength;
+            this.Stamina = stamina;
+        }
+    }
+}
---
+++ BossFightGame/Program.cs
@@ -0,0 +1,18 @@
+﻿using BossFightGame;
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+var randomStrength = new Random().Next(0 - 31);
+var Hero = new GameCharacter(100, 20, 40);
+var Boss = new GameCharacter(400, randomStrength, 10);
+play();
+void play()
+{
+    var CurrentPlayer = "hero";
+    Console.WriteLine("welcome to Bossfight! You can keep playing until one character loses all its health.");
+    while (Hero.Health != 0 || Boss.Health != 0)
+    {
+
+
+    }
+}
+
