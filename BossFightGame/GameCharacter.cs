fd997348e5f8a2545b3416db2713a4a59bb76fde
imanahmedoslo <imanahmedimanahmed@gmail.com>
2023-05-22 01:25:55 +0200

    done with task

--- BossFightGame/GameCharacter.cs
+++ BossFightGame/GameCharacter.cs
@@ -8,14 +8,64 @@ namespace BossFightGame
 {
     public class GameCharacter
     {
-        public int Health { get; set; }
-        public int Strength { get; set; }
-        public int Stamina { get; set; }
+        public int Health { get; private set; }
+        public int Strength { get;private set; }
+        public int Stamina { get; private set; }
+       public string Name { get; private set; }
+        public bool IsMyTurn { get; private set; }
 
-        public GameCharacter(int health, int strength, int stamina) { 
+        public GameCharacter(int health, int strength, int stamina, string name, bool isMyTurn) { 
             this.Health = health;   
             this.Strength = strength;
             this.Stamina = stamina;
+            this.Name = name;
+            this.IsMyTurn = isMyTurn;
+        }
+        public GameCharacter(int health, int stamina, string name, bool isMyTurn) { 
+        
+            this.Health = health;
+            this.Stamina = stamina;
+            this.Name = name;
+            randomizeStrength();
+            this.IsMyTurn = isMyTurn;
+        }
+        public void Fight(GameCharacter opponent, int startStamina)
+        {
+            if (this.Stamina == 0)
+            {
+                Recharge(startStamina);
+                this.IsMyTurn = false;
+                opponent.Changetrun();
+            }
+            else {
+                opponent.changeHealth(this);
+                this.Stamina -= 10;
+                this.IsMyTurn = false;
+                opponent.Changetrun();
+               // Console.WriteLine($"{this.Name} attacked");
+            };
+        }
+        public void Recharge(int startStamina)
+        {
+            this.Stamina = startStamina;
+            Console.WriteLine($"{this.Name} was too tired, this round is used for recharge. {this.Name} stamina is now at {startStamina}");
+        }
+
+        public void changeHealth(GameCharacter opponent)
+        {
+            this.Health = this.Health- opponent.Strength;
+            Console.WriteLine($"{this.Name} lost {opponent.Strength}  and has now {this.Health} in health");
+
+        }
+
+        public int randomizeStrength()
+        {
+            return  this.Strength=new Random().Next(0, 31);
+        }
+        public void Changetrun()
+        {
+            this.IsMyTurn = true;
         }
     }
+
 }
--- BossFightGame/Program.cs
+++ BossFightGame/Program.cs
@@ -1,18 +1,59 @@
 ﻿using BossFightGame;
+using System.Linq.Expressions;
 // See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
-var randomStrength = new Random().Next(0 - 31);
-var Hero = new GameCharacter(100, 20, 40);
-var Boss = new GameCharacter(400, randomStrength, 10);
+int randomStrength = new Random().Next(0, 31);
+var Hero = new GameCharacter(170, 20, 40,"hero", true);
+var Boss = new GameCharacter(400, 10, "boss", false);
+List<GameCharacter> Players = new List<GameCharacter>();
+Players.Add(Hero);
+Players.Add(Boss);
 play();
 void play()
 {
-    var CurrentPlayer = "hero";
-    Console.WriteLine("welcome to Bossfight! You can keep playing until one character loses all its health.");
-    while (Hero.Health != 0 || Boss.Health != 0)
+    int startStaminaPlayerOne = Players[0].Stamina;
+    int startStaminaPlayerTwo= Players[1].Stamina;
+    
+    Console.WriteLine("welcome to Bossfight! You keep watching until one character loses all its health.");
+    while (Players[0].Health > 0 && Players[1].Health > 0)
     {
+       // var CurrentPlayer = Players[0].Name;
+        if (Players[0].IsMyTurn)
+       {
+            Players[0].Fight(Players[1], startStaminaPlayerOne);
+            Console.WriteLine($"{Players[1].Name} is now playing");
+            
+        } else if (Players[1].IsMyTurn)
+        {
+            Players[1].randomizeStrength();
+            Players[1].Fight(Players[0], startStaminaPlayerTwo);
+         //   CurrentPlayer = Players[0].Name;
+            Console.WriteLine($"{Players[0].Name} is now playing");
+        }
+        
+            
+               
+               
+
+            
+               
+                
 
 
+        
+           
+       // for (int i = 0; i < 10; i++)
+       // {
+       // randomStrength = new Random().Next(0, 31);
+        //Console.WriteLine(Boss.Strength);
+       
+       // }
+
+
+    }
+     if (Players[0].Health <= 0 || Players[1].Health <= 0)
+    {
+        Console.WriteLine($"Game over, {Players[0].Name} is at{Players[0].Health} health, and {Players[1].Name} is at {Players[1].Health} health.");
     }
 }
 
